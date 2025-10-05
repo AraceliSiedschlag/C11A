f11f89430a8868b26919dbb3152254ca2e57a7fa
Araceli Siedschlag <aasiedschlag@mines.edu>
2025-10-05 10:38:37 -0600

    Created negative balance exception

--- src/NegativeBalanceException.java
+++ src/NegativeBalanceException.java
@@ -1,4 +1,37 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
-public class NegativeBalanceException {
+public class NegativeBalanceException extends Exception{
+	double negativeBalance = 0.0;
 
+	public NegativeBalanceException() {
+		super("Error: negative balance");
+		
+	}
+	
+	public NegativeBalanceException(double balance) {
+		super("Amount exceeds balance by " + balance);
+		negativeBalance  = balance;
+		PrintWriter out;
+		try {
+			out = new PrintWriter("logfile.txt");
+			out.println("Amount exceeds balance by " + balance);
+		} catch (FileNotFoundException e) {
+		
+			System.out.println("File could not be found");
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Balance of "+ negativeBalance + " not allowed";
+	}
+
+	
+	public double getNegativeBalance() {
+		return negativeBalance;
+	}
+		
+	
+		
 }
