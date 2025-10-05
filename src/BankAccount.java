19797a394f83989176628b5e141540d0f2064fe7
gwensab <gpsabol7@gmail.com>
2025-10-05 10:41:58 -0600

    set up bankAccount, withdraw, and quickWithdraw

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,25 @@
 
 public class BankAccount {
-
+	private double balance;
+	
+	public BankAccount(double Balance) {
+		this.balance=Balance;
+	}
+	
+	public void withdraw(double withdrawAmount) throws NegativeBalanceException{
+		if (withdrawAmount > balance) {
+			double negativeBalance = balance - withdrawAmount;
+			throw new NegativeBalanceException(negativeBalance);
+		} else {
+			balance = balance - withdrawAmount;
+		}
+	}
+	
+	public void quickWithdraw(double withdrawAmount) throws NegativeBalanceException{
+		if (withdrawAmount > balance) {
+			throw new NegativeBalanceException();
+		} else {
+			balance = balance - withdrawAmount;
+		}
+	}
 }
