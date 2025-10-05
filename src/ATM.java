c3d688b2e7d4d028f21ad744c933b39282ddcd38
Araceli Siedschlag <aasiedschlag@mines.edu>
2025-10-05 10:47:32 -0600

    Created ATM class with BankAccount and called it in main

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,30 @@
 
 public class ATM {
+	BankAccount account;
 
+	public ATM() {
+		this.account = new BankAccount(500);
+	}
+	
+	public void handleTransactions() {
+		try {
+			account.withdraw(600);
+		} catch(NegativeBalanceException e) {
+			System.out.println(e);
+			System.out.println(e.getMessage());
+			
+		}
+		try {
+			account.quickWithdraw(600);
+		} catch(NegativeBalanceException e) {
+			System.out.println(e);
+			System.out.println(e.getMessage());
+			
+		}
+	}
+	
+	public static void main(String[] parameters) {
+		ATM atm = new ATM();
+		atm.handleTransactions();	
+	}
 }
